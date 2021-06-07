4fe98064bf86192e5c1dcc4fb43912b8e36f04ae
AntonFor <formanchuk-pru@rambler.ru>
2021-06-07 22:31:09 +0700

    Commit todo

---
+++ todo/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import reactDom from 'react-dom';
+
+import App from './components/app';
+
+// eslint-disable-next-line react/jsx-filename-extension
+reactDom.render(<App />, document.getElementById('app'));
